70585d4c67e1c8044d16ab77e21ec01810ee9a7e
Dominik277 <dominikdoric277@gmail.com>
2021-02-11 22:47:34 +0100

    Dodan toggle button ali mu jos nije predana funkcionalnost.

--- app/src/main/java/hr/dominik/nkjaki/MainActivity.kt
+++ app/src/main/java/hr/dominik/nkjaki/MainActivity.kt
@@ -2,10 +2,19 @@ package hr.dominik.nkjaki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val drawerToggle = ActionBarDrawerToggle(this,drawer,R.string.open,R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
