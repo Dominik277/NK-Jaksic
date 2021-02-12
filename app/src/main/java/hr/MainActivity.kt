e776e69c91e6eb034edfe56ecf54e04ea0211502
Dominik277 <dominikdoric277@gmail.com>
2021-02-12 13:50:19 +0100

    Implementirano da se otvori nova aktivnost prilikom klika na navigation drawer item.

--- app/src/main/java/hr/MainActivity.kt
+++ app/src/main/java/hr/MainActivity.kt
@@ -1,10 +1,13 @@
 package hr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import hr.aktivnosti.*
 import hr.dominik.nkjaki.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,6 +23,37 @@ class MainActivity : AppCompatActivity() {
         drawerToggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigation_view.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_rezultati ->{
+                    val intent = Intent(this,RezultatiActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_raspored ->{
+                    val intent = Intent(this, RasporedActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_tablica ->{
+                    val intent = Intent(this, TablicaActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_vijesti ->{
+                    val intent = Intent(this, VijestiActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_momcad ->{
+                    val intent = Intent(this,MomcadActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -39,4 +73,7 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+
+
 }
