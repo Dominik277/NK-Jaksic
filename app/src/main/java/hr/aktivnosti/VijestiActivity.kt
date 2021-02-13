073c67e3ea0987ab9862526612a650b76050bc0e
Dominik277 <dominikdoric277@gmail.com>
2021-02-13 19:20:45 +0100

    Implementirano da se otvara NovaVijestActivity.kt aktivnost prilikom klika na action button.

--- app/src/main/java/hr/aktivnosti/VijestiActivity.kt
+++ app/src/main/java/hr/aktivnosti/VijestiActivity.kt
@@ -1,9 +1,12 @@
 package hr.aktivnosti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import hr.NovaVijestActivity
 import hr.dominik.nkjaki.R
 
 class VijestiActivity : AppCompatActivity() {
@@ -19,5 +22,16 @@ class VijestiActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.vijesti_menu,menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.vijestiMenu -> {
+                val intent = Intent(this,NovaVijestActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
