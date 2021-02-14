1aed27120c19a8d4e8787a4ed205f5ef682d17d1
Dominik277 <dominikdoric277@gmail.com>
2021-02-14 18:29:11 +0100

    Implementirano mjenjanje fragmenata u aktivnosti na klik gumba.

--- app/src/main/java/hr/aktivnosti/TablicaActivity.kt
+++ app/src/main/java/hr/aktivnosti/TablicaActivity.kt
@@ -3,6 +3,10 @@ package hr.aktivnosti
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hr.dominik.nkjaki.R
+import hr.fragmenti.RezultatiFragment
+import hr.fragmenti.StrijelciFragment
+import hr.fragmenti.TablicaFragment
+import kotlinx.android.synthetic.main.activity_tablica.*
 
 class TablicaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,39 @@ class TablicaActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbarTablica))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         title = "Tablica"
+
+        val rezultatiFragment = RezultatiFragment()
+        val strijelciFragment = StrijelciFragment()
+        val tablicaFragment = TablicaFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayoutTablica,tablicaFragment)
+            commit()
+        }
+
+        gumbTablica.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica,tablicaFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        gumbNajboljiStrijelci.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica,strijelciFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        gumbRezultati.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica,rezultatiFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
     }
 }
