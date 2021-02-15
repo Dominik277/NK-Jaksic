91fa9401276a48b7ff70cade5c4cb776a55999c9
Dominik277 <dominikdoric277@gmail.com>
2021-02-15 15:06:48 +0100

    Kreiran adapter za fragment od tablice.

---
+++ app/src/main/java/hr/adapteri/TablicaTablicaAdapter.kt
@@ -0,0 +1,27 @@
+package hr.adapteri
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TablicaTablicaAdapter: RecyclerView.Adapter<TablicaTablicaAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TablicaTablicaAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TablicaTablicaAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
