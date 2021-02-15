887757972fa7c9fa8ad181be7f7924e260b11225
Dominik277 <dominikdoric277@gmail.com>
2021-02-15 15:36:33 +0100

    Dovrsen adapter za recyclerview.

--- app/src/main/java/hr/adapteri/TablicaTablicaAdapter.kt
+++ app/src/main/java/hr/adapteri/TablicaTablicaAdapter.kt
@@ -1,27 +1,36 @@
 package hr.adapteri
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import hr.database.TablicaTablica
+import hr.dominik.nkjaki.R
+import kotlinx.android.synthetic.main.jedan_red_tablica.view.*
 
-class TablicaTablicaAdapter: RecyclerView.Adapter<TablicaTablicaAdapter.ViewHolder>() {
+class TablicaTablicaAdapter constructor (private val sveTabliceUBazi: List<TablicaTablica>): RecyclerView.Adapter<TablicaTablicaAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
-
+        val tablicaPozicija: TextView = itemView.tablica_pozicija
+        val tablicaIme: TextView = itemView.tablica_ime
+        val tablicaSusreti: TextView = itemView.tablica_susreti
+        val tablicaGolovi: TextView = itemView.tablica_golovi
+        val tablicaBodovi: TextView = itemView.tablica_bodovi
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): TablicaTablicaAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TablicaTablicaAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.jedan_red_tablica, parent, false)
+        return TablicaTablicaAdapter.ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TablicaTablicaAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.tablica_pozicija.text = sveTabliceUBazi[position].pozicija.toString()
+        holder.itemView.tablica_ime.text = sveTabliceUBazi[position].imeTima
+        holder.itemView.tablica_susreti.text = sveTabliceUBazi[position].odigraniSusreti.toString()
+        holder.itemView.tablica_golovi.text = sveTabliceUBazi[position].golRazlika.toString()
+        holder.itemView.tablica_bodovi.text = sveTabliceUBazi[position].bodovi.toString()
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = sveTabliceUBazi.size
 }
