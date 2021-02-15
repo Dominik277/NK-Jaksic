7eef142255d52d8ed59c527d5625e0827ed7a91d
Dominik277 <dominikdoric277@gmail.com>
2021-02-15 15:22:30 +0100

    Kreiran dao za fragment tablice.

--- app/src/main/java/hr/database/TablicaTablicaDao.kt
+++ app/src/main/java/hr/database/TablicaTablicaDao.kt
@@ -1,4 +1,20 @@
 package hr.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface TablicaTablicaDao {
+
+    @Query("SELECT * FROM tablica_tablica ORDER BY pozicija desc")
+    fun getTablicaTablica(): List<TablicaTablica>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTablicaTablica(tablica_tablica: TablicaTablica)
+
+    @Query("DELETE FROM tablica_tablica")
+    fun deleteTablicaTablica()
+
 }
