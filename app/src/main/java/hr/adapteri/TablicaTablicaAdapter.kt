53a9516795c458a0a63002118bac649e8c4fe7b3
Dominik277 <dominikdoric277@gmail.com>
2021-02-16 22:59:47 +0100

    Implementiran adapter za recyclerView.

---
+++ app/src/main/java/hr/adapteri/TablicaTablicaAdapter.kt
@@ -0,0 +1,26 @@
+package hr.adapteri
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import hr.database.TablicaTablica
+
+class TablicaTablicaAdapter(private val sveTabliceUBazi: List<TablicaTablica>) : RecyclerView.Adapter<TablicaTablicaAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    class ViewHolder {
+
+    }
+
+}
