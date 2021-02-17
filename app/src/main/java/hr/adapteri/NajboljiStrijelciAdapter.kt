a7445f0f7771cfa89d73d358cc48d0f587cc1d23
Dominik277 <dominikdoric277@gmail.com>
2021-02-17 12:59:02 +0100

    Implementirane sve metode i klase za NajboljiStrijelciAdapter.kt.

--- app/src/main/java/hr/adapteri/NajboljiStrijelciAdapter.kt
+++ app/src/main/java/hr/adapteri/NajboljiStrijelciAdapter.kt
@@ -1,4 +1,29 @@
 package hr.adapteri
 
-class NajboljiStrijelciAdapterK {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import hr.database.table.NajboljiStrijelci
+
+class NajboljiStrijelciAdapter(private val sviStrijelciUBazi: List<NajboljiStrijelci>) : RecyclerView.Adapter<NajboljiStrijelciAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NajboljiStrijelciAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NajboljiStrijelciAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
 }
