8f0a2c619f7524e63fcd40eee553ea65905a67c4
Dominik277 <dominikdoric277@gmail.com>
2021-02-17 12:51:09 +0100

    Kreirana tablica za najboljeg strijelca i dao za najboljeg strijelca.

--- app/src/main/java/hr/database/dao/NajboljiStrijelciDao.kt
+++ app/src/main/java/hr/database/dao/NajboljiStrijelciDao.kt
@@ -1,4 +1,22 @@
 package hr.database.dao
 
-class NajboljiStrijelciDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import hr.database.table.Igraci
+import hr.database.table.NajboljiStrijelci
+
+@Dao
+interface NajboljiStrijelciDao {
+
+    @Query("SELECT * FROM najbolji_strijelci ORDER BY pozicija_po_golovima asc")
+    fun getNajboljiStrijelciData(): List<NajboljiStrijelci>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNajboljiStrijelac(najboljiStrijelac: NajboljiStrijelci): Long
+
+    @Query("DELETE FROM najbolji_strijelci")
+    fun deleteNajboljiStrijelci()
+
 }
--- app/src/main/java/hr/database/table/NajboljiStrijelci.kt
+++ app/src/main/java/hr/database/table/NajboljiStrijelci.kt
@@ -1,4 +1,21 @@
 package hr.database.table
 
-class NajboljiStrijelci {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "najbolji_strijelci")
+data class NajboljiStrijelci (
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+
+    @ColumnInfo(name = "pozicija_po_golovima")
+    val pozicijaPoGolovima: Int,
+
+    @ColumnInfo(name = "ime_igraca")
+    val imeIgraca: String,
+
+    @ColumnInfo(name = "broj_golova")
+    val brojGolova: String
+        )
