e726ba5cef7e4b980223a9735e8c8245b2b68b01
Dominik277 <dominikdoric277@gmail.com>
2021-02-17 13:12:51 +0100

    Dovrsen adapter za NajboljiStrijelciAdapter.kt.

--- app/src/main/java/hr/adapteri/NajboljiStrijelciAdapter.kt
+++ app/src/main/java/hr/adapteri/NajboljiStrijelciAdapter.kt
@@ -1,29 +1,33 @@
 package hr.adapteri
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hr.database.table.NajboljiStrijelci
+import hr.dominik.nkjaki.R
+import kotlinx.android.synthetic.main.jedan_red_najbolji_strijelci.view.*
 
 class NajboljiStrijelciAdapter(private val sviStrijelciUBazi: List<NajboljiStrijelci>) : RecyclerView.Adapter<NajboljiStrijelciAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): NajboljiStrijelciAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NajboljiStrijelciAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.jedan_red_najbolji_strijelci, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NajboljiStrijelciAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.strijelci_pozicija.text = sviStrijelciUBazi[position].pozicijaPoGolovima.toString()
+        holder.itemView.strijecli_ime.text = sviStrijelciUBazi[position].imeIgraca
+        holder.itemView.strijelci_golovi.text = sviStrijelciUBazi[position].brojGolova
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = sviStrijelciUBazi.size
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val strijelciPozicija: TextView = itemView.strijelci_pozicija
+        val strijelciIme: TextView = itemView.strijecli_ime
+        val strijelciGolovi: TextView = itemView.strijelci_golovi
     }
 
 }
