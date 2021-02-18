a9d8c4340bc3ca55c9858f1c5d8206a634e5486a
Dominik277 <dominikdoric277@gmail.com>
2021-02-18 11:36:12 +0100

    Kreiran kostur adaptera za recyclerview.

--- app/src/main/java/hr/adapteri/RasporedAdapter.kt
+++ app/src/main/java/hr/adapteri/RasporedAdapter.kt
@@ -1,4 +1,26 @@
 package hr.adapteri
 
-class RasporedAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import hr.database.table.Raspored
+
+class RasporedAdapter(private val sviRasporediUBazi: List<Raspored>): RecyclerView.Adapter<RasporedAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RasporedAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RasporedAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
 }
