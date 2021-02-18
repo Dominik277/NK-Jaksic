ff41f0758d4f09adfbfbf3e58edb576324ec4f99
Dominik277 <dominikdoric277@gmail.com>
2021-02-18 12:13:06 +0100

    Uneseni rasporedi u bazu i prikazani unutar recyclerview-a.

--- app/src/main/java/hr/aktivnosti/RasporedActivity.kt
+++ app/src/main/java/hr/aktivnosti/RasporedActivity.kt
@@ -2,7 +2,14 @@ package hr.aktivnosti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import hr.adapteri.RasporedAdapter
+import hr.database.NKJaksicDatabase
+import hr.database.table.Raspored
 import hr.dominik.nkjaki.R
+import kotlinx.android.synthetic.main.activity_raspored.*
 
 class RasporedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +18,31 @@ class RasporedActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbarRaspored))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         title="Raspored"
+
+        recyclerviewRaspored.addItemDecoration(DividerItemDecoration
+            (recyclerviewRaspored.context,DividerItemDecoration.VERTICAL))
+
+        val database = Room.databaseBuilder(
+            this, NKJaksicDatabase::class.java,"nk_jaksic_baza"
+        )
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+
+/*
+        database.rasporedDao().insertRaspored(Raspored(0,"24.02","NK Jakšić-NK Kuzmica"))
+        database.rasporedDao().insertRaspored(Raspored(1,"28.02","NK Jakšić-NK Požega"))
+        database.rasporedDao().insertRaspored(Raspored(2,"02.03","NK Jakšić-NK Vidovci"))
+        database.rasporedDao().insertRaspored(Raspored(3,"14.03","NK Jakšić-NK Trenkovo"))
+        database.rasporedDao().insertRaspored(Raspored(4,"24.03","NK Jakšić-NK Mihaljevci"))
+*/
+        //database.rasporedDao().deletePodatkeRaspored()
+        val sviRasporediUBaziPodataka = database.rasporedDao().getRasporedData()
+
+        recyclerviewRaspored.apply {
+            layoutManager = LinearLayoutManager(this@RasporedActivity)
+            adapter = RasporedAdapter(sviRasporediUBaziPodataka)
+        }
+
     }
 }
