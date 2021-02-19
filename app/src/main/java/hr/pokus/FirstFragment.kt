b50c840ae4bf9bdb25125a53b326e09f9877e968
Dominik277 <dominikdoric277@gmail.com>
2021-02-19 17:15:06 +0100

    Kreirana logika za prvi i drugi fragment.

--- app/src/main/java/hr/pokus/FirstFragment.kt
+++ app/src/main/java/hr/pokus/FirstFragment.kt
@@ -1,9 +1,59 @@
 package hr.pokus
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import hr.dominik.nkjaki.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
-class FirstFragment : Fragment(){
+class FirstFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ExampleAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
 
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        val exampleList = generateDummyList(500)
+
+        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(context)
+        recycler_view.setHasFixedSize(true)
+
+    }
+
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_launcher_background
+                1 -> R.drawable.ic_launcher_background
+                else -> R.drawable.ic_launcher_background
+            }
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
+    }
 
 }
