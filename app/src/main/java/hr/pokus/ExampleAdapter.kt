35eaf6566fd8ba54a2a3e106496382c07580a176
Dominik277 <dominikdoric277@gmail.com>
2021-02-19 17:04:19 +0100

    Napravljen vizualni dio za jedan card view i dovrsen adapter.

--- app/src/main/java/hr/pokus/ExampleAdapter.kt
+++ app/src/main/java/hr/pokus/ExampleAdapter.kt
@@ -3,8 +3,11 @@ package hr.pokus
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hr.dominik.nkjaki.R
+import kotlinx.android.synthetic.main.example_item.view.*
 
 class ExampleAdapter(private val exampleList: List<ExampleItem>): RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleAdapter.ExampleViewHolder {
@@ -24,7 +27,9 @@ class ExampleAdapter(private val exampleList: List<ExampleItem>): RecyclerView.A
     override fun getItemCount() = exampleList.size
 
     class ExampleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val imageView: ImageView = itemView.image_view
+        val textView1: TextView = itemView.text_view_1
+        val textView2: TextView = itemView.text_view_2
     }
 
 
