d0919e84ea6bd4e5815a272abfe2e8fe3084e79c
Dominik277 <dominikdoric277@gmail.com>
2021-02-20 14:13:18 +0100

    Dodano sve potrebno kako bi u raspored dodao fragment.

---
+++ app/src/main/java/hr/pokusRaspored/PokusRasporedActivity.kt
@@ -0,0 +1,12 @@
+package hr.pokusRaspored
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominik.nkjaki.R
+
+class PokusRasporedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokus_raspored)
+    }
+}
