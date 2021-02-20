9a648fee9f3d615fa58cbf5691e7481b4358ba1b
Dominik277 <dominikdoric277@gmail.com>
2021-02-20 16:37:21 +0100

    Dodano sve potrebno za izgradnju fragmenta unutar rezultati aktivnosti.

---
+++ app/src/main/java/hr/pokusRezultati/PokusRezultatiActivity.kt
@@ -0,0 +1,12 @@
+package hr.pokusRezultati
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominik.nkjaki.R
+
+class PokusRezultatiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokus_rezultati)
+    }
+}
