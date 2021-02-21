3a0cdcbb3e624593f3edc0666dc107663465def7
Dominik277 <dominikdoric277@gmail.com>
2021-02-21 18:39:26 +0100

    Dodano sve sto mi je potrebno za pokus.

---
+++ app/src/main/java/hr/pokusTablicaFragmenti/PokusRasporedFragment.kt
@@ -0,0 +1,4 @@
+package hr.pokusTablicaFragmenti
+
+class PokusRasporedFragment {
+}
---
+++ app/src/main/java/hr/pokusTablicaFragmenti/PokusTablicaActivity.kt
@@ -0,0 +1,12 @@
+package hr.pokusTablicaFragmenti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominik.nkjaki.R
+
+class PokusTablicaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokus_tablica)
+    }
+}
