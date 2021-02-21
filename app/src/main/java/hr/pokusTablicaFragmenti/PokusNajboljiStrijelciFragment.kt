0610101d23b05fe1ea105351eb31a7bee96eabd9
Dominik277 <dominikdoric277@gmail.com>
2021-02-21 19:43:23 +0100

    Dodano dosta toga.

--- app/src/main/java/hr/pokusTablicaFragmenti/PokusNajboljiStrijelciFragment.kt
+++ app/src/main/java/hr/pokusTablicaFragmenti/PokusNajboljiStrijelciFragment.kt
@@ -1,4 +1,11 @@
 package hr.pokusTablicaFragmenti
 
-class PokusNajboljiStrijelciFragment {
+
+import androidx.fragment.app.Fragment
+import hr.dominik.nkjaki.R
+
+class PokusNajboljiStrijelciFragment : Fragment(R.layout.pokus_fragment_najbolji_strijelci) {
+
+
+
 }
--- app/src/main/java/hr/pokusTablicaFragmenti/PokusTablicaFragment.kt
+++ app/src/main/java/hr/pokusTablicaFragmenti/PokusTablicaFragment.kt
@@ -8,18 +8,8 @@ import androidx.fragment.app.Fragment
 import hr.adapteri.NajboljiStrijelciAdapter
 import hr.dominik.nkjaki.R
 
-class PokusTablicaFragment : Fragment() {
+class PokusTablicaFragment : Fragment(R.layout.pokus_fragment_tablica) {
 
-    //private lateinit var recyclerViewNajboljiStrijelci: RecyclerView
-    private lateinit var adapter: NajboljiStrijelciAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_najbolji_strijelci, container, false)
-    }
 
 }
