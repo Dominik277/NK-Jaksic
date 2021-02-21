2c492fd32cbc35491a978d5d7099a5b573656848
Dominik277 <dominikdoric277@gmail.com>
2021-02-21 19:09:38 +0100

    Dodan izgled za tablica aktivnost.

--- app/src/main/java/hr/pokusTablicaFragmenti/PokusTablicaFragment.kt
+++ app/src/main/java/hr/pokusTablicaFragmenti/PokusTablicaFragment.kt
@@ -1,4 +1,25 @@
 package hr.pokusTablicaFragmenti
 
-class PokusTablicaFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import hr.adapteri.NajboljiStrijelciAdapter
+import hr.dominik.nkjaki.R
+
+class PokusTablicaFragment : Fragment() {
+
+    //private lateinit var recyclerViewNajboljiStrijelci: RecyclerView
+    private lateinit var adapter: NajboljiStrijelciAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_najbolji_strijelci, container, false)
+    }
+
 }
