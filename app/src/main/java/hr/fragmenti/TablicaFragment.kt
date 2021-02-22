d47db62b9790d271f07f65366332d7fef99f306b
Dominik277 <dominikdoric277@gmail.com>
2021-02-22 11:37:50 +0100

    Napooooookon implementiran pattern single activity multiple fragments.

--- app/src/main/java/hr/fragmenti/TablicaFragment.kt
+++ app/src/main/java/hr/fragmenti/TablicaFragment.kt
@@ -1,10 +1,56 @@
 package hr.fragmenti
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import hr.dominik.nkjaki.R
+import kotlinx.android.synthetic.main.fragment_tablica.*
 
 class TablicaFragment: Fragment(R.layout.fragment_tablica) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val tablicaTablicaFragment = TablicaTablicaFragment()
+        val strijelciFragment = NajboljiStrijelciFragment()
+        val tablicaRezultatiFragment = TablicaRezultatiFragment()
+        val tablicaRasporedFragment = TablicaRasporedFragment()
 
+        childFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayoutTablica, tablicaTablicaFragment)
+            commit()
+        }
+
+        gumbTablica.setOnClickListener {
+            childFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica, tablicaTablicaFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        gumbNajboljiStrijelci.setOnClickListener {
+            childFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica, strijelciFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        gumbRaspored.setOnClickListener {
+            childFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica, tablicaRasporedFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        gumbRezultati.setOnClickListener {
+            childFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutTablica, tablicaRezultatiFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
 }
