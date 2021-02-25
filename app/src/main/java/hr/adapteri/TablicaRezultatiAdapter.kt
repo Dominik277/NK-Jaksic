a11659693317b1508d7951f7be62797890bc539b
Dominik277 <dominikdoric277@gmail.com>
2021-02-25 15:19:21 +0100

    Jos par imena promjenjeno.

--- app/src/main/java/hr/adapteri/TablicaRezultatiAdapter.kt
+++ app/src/main/java/hr/adapteri/TablicaRezultatiAdapter.kt
@@ -16,35 +16,35 @@ class TablicaRezultatiAdapter(private val sviNoviRezultatiUBazi: List<TablicaRez
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.novi_tv_brojKola.text = sviNoviRezultatiUBazi[position].novi_brojKola.toString()
-        holder.itemView.novi_tv_prvaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaDatum
-        holder.itemView.novi_tv_domacinPrvaUtakmica.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaDomacin
-        holder.itemView.novi_tv_gostPrvaUtakmica.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaGost
-        holder.itemView.novi_tv_rezultatPrvaUtakmica.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaRezultat
-        holder.itemView.novi_tv_drugaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaDatum
-        holder.itemView.novi_tv_domacinDrugaUtakmica.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaDomacin
-        holder.itemView.novi_tv_gostDrugaUtakmica.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaGost
-        holder.itemView.novi_tv_rezultatDrugaUtakmica.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaRezultat
-        holder.itemView.novi_tv_trecaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaDatum
-        holder.itemView.novi_tv_domacinTrecaUtakmica.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaDomacin
-        holder.itemView.novi_tv_gostTrecaUtakmica.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaGost
-        holder.itemView.novi_tv_rezultatTrecaUtakmica.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaRezultat
-        holder.itemView.novi_tv_cetvrtaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaDatum
-        holder.itemView.novi_tv_domacinCetvrtaUtakmica.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaDomacin
-        holder.itemView.novi_tv_gostCetvrtaUtakmica.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaGost
-        holder.itemView.novi_tv_rezultatCetvrtaUtakmica.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaRezultat
-        holder.itemView.novi_tv_petaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaDatum
-        holder.itemView.novi_tv_domacinPetaUtakmica.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaDomacin
-        holder.itemView.novi_tv_gostPetaUtakmica.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaGost
-        holder.itemView.novi_tv_rezultatPetaUtakmica.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaRezultat
-        holder.itemView.novi_tv_sestaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaDatum
-        holder.itemView.novi_tv_domacinSestaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaDomacin
-        holder.itemView.novi_tv_gostSestaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaGost
-        holder.itemView.novi_tv_rezultatSestaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaRezultat
-        holder.itemView.novi_tv_sedmaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaDatum
-        holder.itemView.novi_tv_domacinSedmaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaDomacin
-        holder.itemView.novi_tv_gostSedmaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaGost
-        holder.itemView.novi_tv_rezultatSedmaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_brojKola.text = sviNoviRezultatiUBazi[position].novi_brojKola.toString()
+        holder.itemView.tablicaRezultati_prvaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinPrvaUtakmica.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostPrvaUtakmica.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatPrvaUtakmica.text = sviNoviRezultatiUBazi[position].novi_prvaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_drugaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinDrugaUtakmica.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostDrugaUtakmica.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatDrugaUtakmica.text = sviNoviRezultatiUBazi[position].novi_drugaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_trecaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinTrecaUtakmica.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostTrecaUtakmica.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatTrecaUtakmica.text = sviNoviRezultatiUBazi[position].novi_trecaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_cetvrtaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinCetvrtaUtakmica.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostCetvrtaUtakmica.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatCetvrtaUtakmica.text = sviNoviRezultatiUBazi[position].novi_cetvrtaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_petaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinPetaUtakmica.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostPetaUtakmica.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatPetaUtakmica.text = sviNoviRezultatiUBazi[position].novi_petaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_sestaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinSestaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostSestaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatSestaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sestaUtakmicaRezultat
+        holder.itemView.tablicaRezultati_sedmaUtakmica_datum.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaDatum
+        holder.itemView.tablicaRezultati_domacinSedmaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaDomacin
+        holder.itemView.tablicaRezultati_gostSedmaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaGost
+        holder.itemView.tablicaRezultati_rezultatSedmaUtakmica.text = sviNoviRezultatiUBazi[position].novi_sedmaUtakmicaRezultat
     }
 
     override fun getItemCount() = sviNoviRezultatiUBazi.size
