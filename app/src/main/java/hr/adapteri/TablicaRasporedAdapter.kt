50f22343b0bcec3440c5f243daa52e78542366ac
Dominik277 <dominikdoric277@gmail.com>
2021-02-25 15:12:40 +0100

    Dosta imena promjenjeno.

--- app/src/main/java/hr/adapteri/TablicaRasporedAdapter.kt
+++ app/src/main/java/hr/adapteri/TablicaRasporedAdapter.kt
@@ -16,34 +16,34 @@ class TablicaRasporedAdapter(private val sviNoviRasporediUBazi: List<TablicaRasp
     }
 
     override fun onBindViewHolder(holder: TablicaRasporedAdapter.ViewHolder, position: Int) {
-        holder.itemView.tv_brojKola.text = sviNoviRasporediUBazi[position].brojKola.toString()
-        holder.itemView.tv_prvaUtakmica_datum.text = sviNoviRasporediUBazi[position].prvaUtakmicaDatum
-        holder.itemView.tv_domacinPrvaUtakmica.text = sviNoviRasporediUBazi[position].prvaUtakmicaDomacin
-        holder.itemView.tv_gostPrvaUtakmica.text = sviNoviRasporediUBazi[position].prvaUtakmicaGost
-        holder.itemView.tv_drugaUtakmica_datum.text = sviNoviRasporediUBazi[position].drugaUtakmicaDatum
-        holder.itemView.tv_domacinDrugaUtakmica.text = sviNoviRasporediUBazi[position].drugaUtakmicaDomacin
-        holder.itemView.tv_gostDrugaUtakmica.text = sviNoviRasporediUBazi[position].drugaUtakmicaGost
-        holder.itemView.tv_vrijemeDrugaUtakmica.text = sviNoviRasporediUBazi[position].drugaUtakmicaVrijeme
-        holder.itemView.tv_trecaUtakmica_datum.text = sviNoviRasporediUBazi[position].trecaUtakmicaDatum
-        holder.itemView.tv_domacinTrecaUtakmica.text = sviNoviRasporediUBazi[position].trecaUtakmicaDomacin
-        holder.itemView.tv_gostTrecaUtakmica.text = sviNoviRasporediUBazi[position].trecaUtakmicaGost
-        holder.itemView.tv_vrijemeTrecaUtakmica.text = sviNoviRasporediUBazi[position].trecaUtakmicaVrijeme
-        holder.itemView.tv_cetvrtaUtakmica_datum.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaDatum
-        holder.itemView.tv_domacinCetvrtaUtakmica.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaDomacin
-        holder.itemView.tv_gostCetvrtaUtakmica.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaGost
-        holder.itemView.tv_vrijemeCetvrtaUtakmica.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaVrijeme
-        holder.itemView.tv_petaUtakmica_datum.text = sviNoviRasporediUBazi[position].petaUtakmicaDatum
-        holder.itemView.tv_domacinPetaUtakmica.text = sviNoviRasporediUBazi[position].petaUtakmicaDomacin
-        holder.itemView.tv_gostPetaUtakmica.text = sviNoviRasporediUBazi[position].petaUtakmicaGost
-        holder.itemView.tv_vrijemePetaUtakmica.text = sviNoviRasporediUBazi[position].petaUtakmicaVrijeme
-        holder.itemView.tv_sestaUtakmica_datum.text = sviNoviRasporediUBazi[position].sestaUtakmicaDatum
-        holder.itemView.tv_domacinSestaUtakmica.text = sviNoviRasporediUBazi[position].sestaUtakmicaDomacin
-        holder.itemView.tv_gostSestaUtakmica.text = sviNoviRasporediUBazi[position].sestaUtakmicaGost
-        holder.itemView.tv_vrijemeSestaUtakmica.text = sviNoviRasporediUBazi[position].sestaUtakmicaVrijeme
-        holder.itemView.tv_sedmaUtakmica_datum.text = sviNoviRasporediUBazi[position].sedmaUtakmicaDatum
-        holder.itemView.tv_domacinSedmaUtakmica.text = sviNoviRasporediUBazi[position].sedmaUtakmicaDomacin
-        holder.itemView.tv_gostSedmaUtakmica.text = sviNoviRasporediUBazi[position].sedmaUtakmicaGost
-        holder.itemView.tv_vrijemeSedmaUtakmica.text = sviNoviRasporediUBazi[position].sedmaUtakmicaVrijeme
+        holder.itemView.tablicaRaspored_brojKola.text = sviNoviRasporediUBazi[position].brojKola.toString()
+        holder.itemView.tablicaRaspored_prvaUtakmica_datum.text = sviNoviRasporediUBazi[position].prvaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinPrvaUtakmica.text = sviNoviRasporediUBazi[position].prvaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostPrvaUtakmica.text = sviNoviRasporediUBazi[position].prvaUtakmicaGost
+        holder.itemView.tablicaRaspored_drugaUtakmica_datum.text = sviNoviRasporediUBazi[position].drugaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinDrugaUtakmica.text = sviNoviRasporediUBazi[position].drugaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostDrugaUtakmica.text = sviNoviRasporediUBazi[position].drugaUtakmicaGost
+        holder.itemView.tablicaRaspored_vrijemeDrugaUtakmica.text = sviNoviRasporediUBazi[position].drugaUtakmicaVrijeme
+        holder.itemView.tablicaRaspored_trecaUtakmica_datum.text = sviNoviRasporediUBazi[position].trecaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinTrecaUtakmica.text = sviNoviRasporediUBazi[position].trecaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostTrecaUtakmica.text = sviNoviRasporediUBazi[position].trecaUtakmicaGost
+        holder.itemView.tablicaRaspored_vrijemeTrecaUtakmica.text = sviNoviRasporediUBazi[position].trecaUtakmicaVrijeme
+        holder.itemView.tablicaRaspored_cetvrtaUtakmica_datum.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinCetvrtaUtakmica.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostCetvrtaUtakmica.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaGost
+        holder.itemView.tablicaRaspored_vrijemeCetvrtaUtakmica.text = sviNoviRasporediUBazi[position].cetvrtaUtakmicaVrijeme
+        holder.itemView.tablicaRaspored_petaUtakmica_datum.text = sviNoviRasporediUBazi[position].petaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinPetaUtakmica.text = sviNoviRasporediUBazi[position].petaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostPetaUtakmica.text = sviNoviRasporediUBazi[position].petaUtakmicaGost
+        holder.itemView.tablicaRaspored_vrijemePetaUtakmica.text = sviNoviRasporediUBazi[position].petaUtakmicaVrijeme
+        holder.itemView.tablicaRaspored_sestaUtakmica_datum.text = sviNoviRasporediUBazi[position].sestaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinSestaUtakmica.text = sviNoviRasporediUBazi[position].sestaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostSestaUtakmica.text = sviNoviRasporediUBazi[position].sestaUtakmicaGost
+        holder.itemView.tablicaRaspored_vrijemeSestaUtakmica.text = sviNoviRasporediUBazi[position].sestaUtakmicaVrijeme
+        holder.itemView.tablicaRaspored_sedmaUtakmica_datum.text = sviNoviRasporediUBazi[position].sedmaUtakmicaDatum
+        holder.itemView.tablicaRaspored_domacinSedmaUtakmica.text = sviNoviRasporediUBazi[position].sedmaUtakmicaDomacin
+        holder.itemView.tablicaRaspored_gostSedmaUtakmica.text = sviNoviRasporediUBazi[position].sedmaUtakmicaGost
+        holder.itemView.tablicaRaspored_vrijemeSedmaUtakmica.text = sviNoviRasporediUBazi[position].sedmaUtakmicaVrijeme
 
     }
 
