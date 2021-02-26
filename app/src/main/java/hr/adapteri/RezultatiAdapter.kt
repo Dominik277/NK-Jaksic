151af02d2dab91b2b8351f5a2a15f13c24ede3d3
Dominik277 <dominikdoric277@gmail.com>
2021-02-26 17:29:38 +0100

    Dovrseno sve u vezi rezultata.

--- app/src/main/java/hr/adapteri/RezultatiAdapter.kt
+++ app/src/main/java/hr/adapteri/RezultatiAdapter.kt
@@ -4,11 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import hr.database.table.Raspored
 import hr.database.table.Rezultat
 import hr.dominik.nkjaki.R
 import kotlinx.android.synthetic.main.jedan_red_rezultati.view.*
 
-class RezultatiAdapter(private val sviRezultatiUBazi: List<Rezultat>): RecyclerView.Adapter<RezultatiAdapter.ViewHolder>() {
+class RezultatiAdapter: RecyclerView.Adapter<RezultatiAdapter.ViewHolder>() {
+
+    private var rezultatiList = emptyList<Rezultat>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.jedan_red_rezultati, parent, false)
@@ -16,17 +19,26 @@ class RezultatiAdapter(private val sviRezultatiUBazi: List<Rezultat>): RecyclerV
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.natjecanjeRezultati.text = sviRezultatiUBazi[position].natjecanjeRezultat
-        holder.itemView.datumRezultati.text = sviRezultatiUBazi[position].datumRezultat
-        holder.itemView.domacinRezultati.text = sviRezultatiUBazi[position].domacinRezultat
-        holder.itemView.gostRezultati.text = sviRezultatiUBazi[position].gostRezultat
-        holder.itemView.rezultatRezultati.text = sviRezultatiUBazi[position].rezultatUtakmice
-        holder.itemView.ishodRezultati.text = sviRezultatiUBazi[position].ishodRezultat
+        val currentItem = rezultatiList[position]
+        holder.itemView.natjecanjeRezultati.text = currentItem.natjecanjeRezultat
+        holder.itemView.datumRezultati.text = currentItem.datumRezultat
+        holder.itemView.domacinRezultati.text = currentItem.domacinRezultat
+        holder.itemView.gostRezultati.text = currentItem.gostRezultat
+        holder.itemView.rezultatRezultati.text = currentItem.rezultatUtakmice
+        holder.itemView.ishodRezultati.text = currentItem.ishodRezultat
     }
 
-    override fun getItemCount() = sviRezultatiUBazi.size
+    override fun getItemCount(): Int{
+        return rezultatiList.size
+    }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     }
+
+    fun setData(rezultat: List<Rezultat>){
+        this.rezultatiList = rezultat
+        notifyDataSetChanged()
+    }
+
 }
--- app/src/main/java/hr/fragmenti/RezultatiFragment.kt
+++ app/src/main/java/hr/fragmenti/RezultatiFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
@@ -12,24 +14,44 @@ import hr.adapteri.RezultatiAdapter
 import hr.database.NKJaksicDatabase
 import hr.database.table.Rezultat
 import hr.dominik.nkjaki.R
+import hr.viewModel.RasporedViewModel
+import hr.viewModel.RezultatViewModel
 import kotlinx.android.synthetic.main.fragment_rezultati.*
+import kotlinx.android.synthetic.main.fragment_rezultati.view.*
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class RezultatiFragment: Fragment() {
 
-    private lateinit var adapter: RezultatiAdapter
+    @InternalCoroutinesApi
+    private lateinit var mRezultatViewModel: RezultatViewModel
 
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rezultati, container, false)
+        val view =  inflater.inflate(R.layout.fragment_rezultati, container, false)
+
+        //RecyclerView
+        val adapter = RezultatiAdapter()
+        val recyclerRezultat = view.recyclerViewRezultati
+        recyclerRezultat.adapter = adapter
+        recyclerRezultat.layoutManager = LinearLayoutManager(requireContext())
+
+        //RezultatiViewModel
+        mRezultatViewModel = ViewModelProvider(this).get(RezultatViewModel::class.java)
+        mRezultatViewModel.readAllDataRezultat.observe(viewLifecycleOwner, Observer { rezultat ->
+            adapter.setData(rezultat)
+        })
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+/*
         val database = getActivity()?.let {
             Room.databaseBuilder(
                 it, NKJaksicDatabase::class.java,"nk_jaksic_baza"
@@ -38,7 +60,7 @@ class RezultatiFragment: Fragment() {
                 .fallbackToDestructiveMigration()
                 .build()
         }
-
+*/
         recyclerViewRezultati.addItemDecoration(DividerItemDecoration
             (recyclerViewRezultati.context,DividerItemDecoration.VERTICAL))
 /*
@@ -55,11 +77,11 @@ class RezultatiFragment: Fragment() {
         database?.rezultatiDao()?.insertRezultat(Rezultat(10,"Prijateljska","21.09","NK Jakšić","NK Kuzmica","1:0","P"))
 */
         //database?.rezultatiDao()?.deletePodatkeRezultat()
-        val exampleRezultatiList = database?.rezultatiDao()?.getRezultatData()
+        //val exampleRezultatiList = database?.rezultatiDao()?.getRezultatData()
 
         //recyclerViewRezultati.adapter = exampleRezultatiList?.let { RezultatiAdapter(it) }
-        recyclerViewRezultati.layoutManager = LinearLayoutManager(context)
-        recyclerViewRezultati.setHasFixedSize(true)
+        //recyclerViewRezultati.layoutManager = LinearLayoutManager(context)
+        //recyclerViewRezultati.setHasFixedSize(true)
 
     }
 
