aad64d12e8781dd888cf1a7b6bee21a07cd12394
Dominik277 <dominikdoric277@gmail.com>
2021-02-26 17:13:33 +0100

    Zavrsen repozitorij za rezultate.

--- app/src/main/java/hr/repository/RezultatRepository.kt
+++ app/src/main/java/hr/repository/RezultatRepository.kt
@@ -1,4 +1,15 @@
 package hr.repository
 
-class RezultatRepository {
+import androidx.lifecycle.LiveData
+import hr.database.dao.RezultatDao
+import hr.database.table.Rezultat
+
+class RezultatRepository(private val rezultatiDao: RezultatDao) {
+
+    val readAllDataRezultat: LiveData<List<Rezultat>> = rezultatiDao.getRezultatData()
+
+    suspend fun addRezultat(rezultat: Rezultat){
+        rezultatiDao.insertRezultat(rezultat)
+    }
+
 }
