c25e1b570aff0b9b2714600ddea3b8e04fe7c3cd
Dominik277 <dominikdoric277@gmail.com>
2021-02-27 19:19:30 +0100

    Gotova i logika za izvacenje podataka iz baze i prikazivanja u recyclerview-u.

--- app/src/main/java/hr/fragmenti/TablicaTablicaFragment.kt
+++ app/src/main/java/hr/fragmenti/TablicaTablicaFragment.kt
@@ -1,8 +1,12 @@
 package hr.fragmenti
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
@@ -10,13 +14,42 @@ import hr.adapteri.TablicaTablicaAdapter
 import hr.database.NKJaksicDatabase
 import hr.database.table.TablicaTablica
 import hr.dominik.nkjaki.R
+import hr.viewModel.TablicaTablicaViewModel
 import kotlinx.android.synthetic.main.tablica_fragment_tablica.*
+import kotlinx.android.synthetic.main.tablica_fragment_tablica.view.*
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class TablicaTablicaFragment : Fragment(R.layout.tablica_fragment_tablica) {
 
+    @InternalCoroutinesApi
+    private lateinit var mTablicaTablicaViewModel: TablicaTablicaViewModel
+
+    @InternalCoroutinesApi
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.tablica_fragment_tablica,container, false)
+
+        //RecyclerView
+        val adapter = TablicaTablicaAdapter()
+        val recyclerTablica = view.tablicaRecyclerViewTablica
+        recyclerTablica.adapter = adapter
+        recyclerTablica.layoutManager = LinearLayoutManager(requireContext())
+
+        //TablicaTablicaViewModel
+        mTablicaTablicaViewModel = ViewModelProvider(this).get(TablicaTablicaViewModel::class.java)
+        mTablicaTablicaViewModel.readAllDataTablicaTablica.observe(viewLifecycleOwner, Observer { tablicaTablica ->
+            adapter.setData(tablicaTablica)
+        })
+
+        return view
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+/*
         val database = getActivity()?.let {
             Room.databaseBuilder(
                 it, NKJaksicDatabase::class.java,"nk_jaksic_baza"
@@ -25,7 +58,7 @@ class TablicaTablicaFragment : Fragment(R.layout.tablica_fragment_tablica) {
                 .fallbackToDestructiveMigration()
                 .build()
         }
-
+*/
         tablicaRecyclerViewTablica.addItemDecoration(DividerItemDecoration
             (tablicaRecyclerViewTablica.context, DividerItemDecoration.VERTICAL))
 /*
