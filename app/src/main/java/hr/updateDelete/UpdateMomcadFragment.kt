d155a9b42e45eee3c18f61b4cd01e23d4d717713
Dominik277 <dominikdoric277@gmail.com>
2021-03-17 20:16:03 +0100

    Dodano dosta toga.

--- app/src/main/java/hr/updateDelete/UpdateMomcadFragment.kt
+++ app/src/main/java/hr/updateDelete/UpdateMomcadFragment.kt
@@ -5,16 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import hr.dominik.nkjaki.R
+import hr.viewModel.MomcadViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class UpdateMomcadFragment: Fragment() {
 
+    private val args by navArgs<UpdateMomcadFragmentArgs>()
+    @InternalCoroutinesApi
+    private lateinit var mMomcadViewModel: MomcadViewModel
+
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.update_momcad_fragment,container,false)
+
+        mMomcadViewModel = ViewModelProvider(this).get(MomcadViewModel::class.java)
 
         return view
     }
--- app/src/main/java/hr/updateDelete/UpdateTablicaTablicaFragment.kt
+++ app/src/main/java/hr/updateDelete/UpdateTablicaTablicaFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import hr.dominik.nkjaki.R
 
 class UpdateTablicaTablicaFragment: Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
