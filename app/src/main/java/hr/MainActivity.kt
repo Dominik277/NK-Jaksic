3b48ccbbf66e7711cd1dcf901b7e0706ab3bf220
Dominik277 <dominikdoric277@gmail.com>
2021-03-17 19:17:28 +0100

    Zavrsena logika za MainActivity.kt.

--- app/src/main/java/hr/MainActivity.kt
+++ app/src/main/java/hr/MainActivity.kt
@@ -7,6 +7,10 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.*
 import hr.dominik.nkjaki.R
 import hr.fragmenti.*
 import hr.novoDodavanje.*
@@ -14,11 +18,38 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_rezultati,R.id.nav_momcad,R.id.nav_pocetnaStranica,
+            R.id.nav_raspored,R.id.nav_tablica,R.id.nav_vijesti),
+            main_drawer_layout
+        )
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
+        navigation_drawer.setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
