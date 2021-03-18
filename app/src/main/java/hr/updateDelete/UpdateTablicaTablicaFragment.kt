374abac6e3660d76c12cc02f1b921a05215ad1fd
Dominik277 <dominikdoric277@gmail.com>
2021-03-18 19:58:09 +0100

    Napravljen update i delete za tablicu.

--- app/src/main/java/hr/updateDelete/UpdateTablicaTablicaFragment.kt
+++ app/src/main/java/hr/updateDelete/UpdateTablicaTablicaFragment.kt
@@ -1,17 +1,29 @@
 package hr.updateDelete
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import hr.database.table.TablicaTablica
 import hr.dominik.nkjaki.R
+import hr.viewModel.TablicaTablicaViewModel
+import kotlinx.android.synthetic.main.update_tablica_tablica_fragment.*
+import kotlinx.android.synthetic.main.update_tablica_tablica_fragment.view.*
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class UpdateTablicaTablicaFragment: Fragment() {
 
+    private val args by navArgs<UpdateTablicaTablicaFragmentArgs>()
+    @InternalCoroutinesApi
+    private lateinit var mTablicaTablicaViewModel: TablicaTablicaViewModel
 
-
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,7 +31,53 @@ class UpdateTablicaTablicaFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.update_tablica_tablica_fragment,container,false)
 
+        mTablicaTablicaViewModel = ViewModelProvider(this).get(TablicaTablicaViewModel::class.java)
+
+
+        view.updateTablicaPozicija.setText(args.updateTablicaArgs.pozicija)
+        view.updateTablicaImeTima.setText(args.updateTablicaArgs.imeTima)
+        view.updateTablicaOdigraniSusreti.setText(args.updateTablicaArgs.odigraniSusreti)
+        view.updateTablicaGolRazlika.setText(args.updateTablicaArgs.golRazlika)
+        view.updateTablicaBodovi.setText(args.updateTablicaArgs.bodovi)
+
+        view.gumbUpdateTablica.setOnClickListener {
+            updateItemTablica()
+        }
+
+        view.gumbDeleteTablica.setOnClickListener {
+            deleteItemTablica()
+        }
+
         return view
+    }
+
+    @InternalCoroutinesApi
+    private fun deleteItemTablica() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){_, _ ->
+            mTablicaTablicaViewModel.deleteTablicaTablica(args.updateTablicaArgs)
+            Toast.makeText(requireContext(),"Brisanje uspješno!", Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_updateTablicaTablicaFragment_to_tablicaTablicaFragment)
+        }
+        builder.setNegativeButton("No"){_, _ -> }
+        builder.setTitle("Delete ?")
+        builder.setMessage("Are you sure you want to delete ?")
+        builder.create().show()
+    }
+
+    @InternalCoroutinesApi
+    private fun updateItemTablica() {
+
+        val pozicijaTablica = updateTablicaPozicija.text.toString()
+        val imeTimaTablica = updateTablicaImeTima.text.toString()
+        val odigraniSusretiTablica = updateTablicaOdigraniSusreti.text.toString()
+        val golRazlikaTablica = updateTablicaGolRazlika.text.toString()
+        val bodovi = updateTablicaBodovi.text.toString()
+
+        val updateTablica = TablicaTablica(args.updateTablicaArgs.id,pozicijaTablica,imeTimaTablica,odigraniSusretiTablica,golRazlikaTablica,bodovi)
+        mTablicaTablicaViewModel.updateTablicaTablica(updateTablica)
+        findNavController().navigate(R.id.action_updateTablicaTablicaFragment_to_tablicaTablicaFragment)
+
     }
 
 }
