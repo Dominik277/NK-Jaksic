c18219b77bf07f0b9ca4a4f63874dda8e75baedc
Dominik277 <dominikdoric277@gmail.com>
2021-03-18 21:57:45 +0100

    Kreirani novi detail fragmenti i spojeni sa njihovim layout fileovima.

---
+++ app/src/main/java/hr/detailFragmenti/DetailRasporedFragment.kt
@@ -0,0 +1,22 @@
+package hr.detailFragmenti
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import hr.dominik.nkjaki.R
+
+class DetailRasporedFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.detail_raspored_fragment,container,false)
+
+        return view
+    }
+
+}
