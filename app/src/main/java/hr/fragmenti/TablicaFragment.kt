7c43d83e542663e1aaa991363c923c43bb3cbad3
Dominik277 <dominikdoric277@gmail.com>
2021-03-18 13:47:40 +0100

    Dodano par stvari.

--- app/src/main/java/hr/fragmenti/TablicaFragment.kt
+++ app/src/main/java/hr/fragmenti/TablicaFragment.kt
@@ -1,13 +1,48 @@
 package hr.fragmenti
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import hr.dominik.nkjaki.R
 import kotlinx.android.synthetic.main.fragment_tablica.*
+import kotlinx.android.synthetic.main.fragment_tablica.view.*
 
-class TablicaFragment: Fragment(R.layout.fragment_tablica) {
+class TablicaFragment: Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_tablica,container,false)
+
+        view.gumbNajboljiStrijelci.setOnClickListener {
+            val action = TablicaFragmentDirections.actionNavTablicaToNajboljiStrijelciFragment()
+            findNavController().navigate(action)
+        }
+
+        view.gumbTablica.setOnClickListener {
+            val action = TablicaFragmentDirections.actionNavTablicaToTablicaTablicaFragment()
+            findNavController().navigate(action)
+        }
+
+        view.gumbRezultati.setOnClickListener {
+            val action = TablicaFragmentDirections.actionNavTablicaToTablicaRezultatiFragment()
+            findNavController().navigate(action)
+        }
+
+        view.gumbRaspored.setOnClickListener {
+            val action = TablicaFragmentDirections.actionNavTablicaToTablicaRasporedFragment()
+            findNavController().navigate(action)
+        }
+
+        return view
+    }
+
+    /*
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -53,4 +88,6 @@ class TablicaFragment: Fragment(R.layout.fragment_tablica) {
             }
         }
     }
+
+     */
 }
