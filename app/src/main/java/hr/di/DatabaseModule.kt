bc4e5d8c4cf851cdbcf05917ca33c687269f0350
Dominik277 <dominikdoric277@gmail.com>
2021-03-24 17:58:24 +0100

    Dodani svi potrebni dependenciji i prilozena singleton instanca baze podataka.

---
+++ app/src/main/java/hr/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package hr.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import hr.database.NKJaksicDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        NKJaksicDatabase::class.java,
+    "nk_jaksic_baza"
+    ).fallbackToDestructiveMigration()
+        .build()
+
+}
