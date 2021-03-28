eb975a5b66d52745e27fd8e799f1db21d62e1ee4
Dominik277 <dominikdoric277@gmail.com>
2021-03-28 22:00:38 +0200

    Prebaceni detail fragmenti u novi paket.

--- app/src/main/java/hr/fragmenti/detailFragmenti/DetailVijestiFragment.kt
+++ app/src/main/java/hr/fragmenti/detailFragmenti/DetailVijestiFragment.kt
@@ -1,14 +1,13 @@
-package hr.detailFragmenti
+package hr.fragmenti.detailFragmenti
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import hr.detailFragmenti.DetailVijestiFragmentArgs
 import hr.dominik.nkjaki.R
-import hr.viewModel.VijestiViewModel
 import kotlinx.android.synthetic.main.detail_vijesti_fragment.view.*
 import kotlinx.coroutines.InternalCoroutinesApi
 
