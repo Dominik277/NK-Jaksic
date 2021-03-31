3572846bd592eefaa583095ff46400a9bfca458b
Dominik277 <dominikdoric277@gmail.com>
2021-03-31 11:08:04 +0200

    Dodan view binding u DodajNoviRasporedLigaFragment.kt.

--- app/src/main/java/hr/fragmenti/novoDodavanje/DodajNoviRasporedLigaFragment.kt
+++ app/src/main/java/hr/fragmenti/novoDodavanje/DodajNoviRasporedLigaFragment.kt
@@ -1,9 +1,7 @@
 package hr.fragmenti.novoDodavanje
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -11,88 +9,107 @@ import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import hr.database.table.TablicaRaspored
 import hr.dominik.nkjaki.R
-import hr.fragmenti.novoDodavanje.DodajNoviRasporedLigaFragmentDirections
+import hr.dominik.nkjaki.databinding.FragmentNoviRasporedLigaBinding
 import hr.viewModel.TablicaRasporedViewModel
-import kotlinx.android.synthetic.main.fragment_novi_raspored_liga.*
-import kotlinx.android.synthetic.main.fragment_novi_raspored_liga.view.*
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @AndroidEntryPoint
-class DodajNoviRasporedLigaFragment: Fragment() {
+class DodajNoviRasporedLigaFragment : Fragment(R.layout.fragment_novi_raspored_liga) {
 
     @InternalCoroutinesApi
     private val mTablicaRasporedViewModel: TablicaRasporedViewModel by viewModels()
+    private lateinit var binding: FragmentNoviRasporedLigaBinding
 
     @InternalCoroutinesApi
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_novi_raspored_liga,container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentNoviRasporedLigaBinding.bind(view)
 
-        view.gumbSpremiTablicaRaspored.setOnClickListener {
+        binding.gumbSpremiTablicaRaspored.setOnClickListener {
             val action =
                 DodajNoviRasporedLigaFragmentDirections.actionDodajNoviRasporedLigaFragmentToTablicaRasporedFragment()
             findNavController().navigate(action)
             insertDataToDatabase()
         }
-        return view
     }
 
     @InternalCoroutinesApi
     private fun insertDataToDatabase() {
 
-        val noviRasporedLigaBrojKola = noviRasporedLigaBrojKola.text.toString()
+        val noviRasporedLigaBrojKola = binding.noviRasporedLigaBrojKola.text.toString()
 
-        val noviRasporedLigaPrviDatum = noviRasporedLigaPrviDatum.text.toString()
-        val noviRasporedLigaPrviDomacin = noviRasporedLigaPrviDomacin.text.toString()
-        val noviRasporedLigaPrviGost = noviRasporedLigaPrviGost.text.toString()
-        val noviRasporedLigaPrvoVrijeme = noviRasporedLigaPrvoVrijeme.text.toString()
+        val noviRasporedLigaPrviDatum = binding.noviRasporedLigaPrviDatum.text.toString()
+        val noviRasporedLigaPrviDomacin = binding.noviRasporedLigaPrviDomacin.text.toString()
+        val noviRasporedLigaPrviGost = binding.noviRasporedLigaPrviGost.text.toString()
+        val noviRasporedLigaPrvoVrijeme = binding.noviRasporedLigaPrvoVrijeme.text.toString()
 
-        val noviRasporedLigaDrugiDatum = noviRasporedLigaDrugiDatum.text.toString()
-        val noviRasporedLigaDrugiDomacin = noviRasporedLigaDrugiDomacin.text.toString()
-        val noviRasporedLigaDrugiGost = noviRasporedLigaDrugiGost.text.toString()
-        val noviRasporedLigaDrugoVrijeme = noviRasporedLigaDrugoVrijeme.text.toString()
+        val noviRasporedLigaDrugiDatum = binding.noviRasporedLigaDrugiDatum.text.toString()
+        val noviRasporedLigaDrugiDomacin = binding.noviRasporedLigaDrugiDomacin.text.toString()
+        val noviRasporedLigaDrugiGost = binding.noviRasporedLigaDrugiGost.text.toString()
+        val noviRasporedLigaDrugoVrijeme = binding.noviRasporedLigaDrugoVrijeme.text.toString()
 
-        val noviRasporedLigaTreciDatum = noviRasporedLigaTreciDatum.text.toString()
-        val noviRasporedLigaTreciDomacin = noviRasporedLigaTreciDomacin.text.toString()
-        val noviRasporedLigaTreciGost = noviRasporedLigaTreciGost.text.toString()
-        val noviRasporedLigaTreceVrijeme = noviRasporedLigaTreceVrijeme.text.toString()
+        val noviRasporedLigaTreciDatum = binding.noviRasporedLigaTreciDatum.text.toString()
+        val noviRasporedLigaTreciDomacin = binding.noviRasporedLigaTreciDomacin.text.toString()
+        val noviRasporedLigaTreciGost = binding.noviRasporedLigaTreciGost.text.toString()
+        val noviRasporedLigaTreceVrijeme = binding.noviRasporedLigaTreceVrijeme.text.toString()
 
-        val noviRasporedLigaCetvrtiDatum = noviRasporedLigaCetvrtiDatum.text.toString()
-        val noviRasporedLigaCetvrtiDomacin = noviRasporedLigaCetvrtiDomacin.text.toString()
-        val noviRasporedLigaCetvrtiGost = noviRasporedLigaCetvrtiGost.text.toString()
-        val noviRasporedLigaCetvrtoVrijeme = noviRasporedLigaCetvrtoVrijeme.text.toString()
+        val noviRasporedLigaCetvrtiDatum = binding.noviRasporedLigaCetvrtiDatum.text.toString()
+        val noviRasporedLigaCetvrtiDomacin = binding.noviRasporedLigaCetvrtiDomacin.text.toString()
+        val noviRasporedLigaCetvrtiGost = binding.noviRasporedLigaCetvrtiGost.text.toString()
+        val noviRasporedLigaCetvrtoVrijeme = binding.noviRasporedLigaCetvrtoVrijeme.text.toString()
 
 
-        val noviRasporedLigaPetiDatum = noviRasporedLigaPetiDatum.text.toString()
-        val noviRasporedLigaPetiDomacin = noviRasporedLigaPetiDomacin.text.toString()
-        val noviRasporedLigaPetiGost = noviRasporedLigaPetiGost.text.toString()
-        val noviRasporedLigaPetoVrijeme = noviRasporedLigaPetoVrijeme.text.toString()
+        val noviRasporedLigaPetiDatum = binding.noviRasporedLigaPetiDatum.text.toString()
+        val noviRasporedLigaPetiDomacin = binding.noviRasporedLigaPetiDomacin.text.toString()
+        val noviRasporedLigaPetiGost = binding.noviRasporedLigaPetiGost.text.toString()
+        val noviRasporedLigaPetoVrijeme = binding.noviRasporedLigaPetoVrijeme.text.toString()
 
-        val noviRasporedLigaSestiDatum = noviRasporedLigaSestiDatum.text.toString()
-        val noviRasporedLigaSestiDomacin = noviRasporedLigaSestiDomacin.text.toString()
-        val noviRasporedLigaSestiGost = noviRasporedLigaSestiGost.text.toString()
-        val noviRasporedLigaSestoVrijeme = noviRasporedLigaSestoVrijeme.text.toString()
+        val noviRasporedLigaSestiDatum = binding.noviRasporedLigaSestiDatum.text.toString()
+        val noviRasporedLigaSestiDomacin = binding.noviRasporedLigaSestiDomacin.text.toString()
+        val noviRasporedLigaSestiGost = binding.noviRasporedLigaSestiGost.text.toString()
+        val noviRasporedLigaSestoVrijeme = binding.noviRasporedLigaSestoVrijeme.text.toString()
 
-        val noviRasporedLigaSedmiDatum = noviRasporedLigaSedmiDatum.text.toString()
-        val noviRasporedLigaSedmiDomacin = noviRasporedLigaSedmiDomacin.text.toString()
-        val noviRasporedLigaSedmiGost = noviRasporedLigaSedmiGost.text.toString()
-        val noviRasporedLigaSedmoVrijeme = noviRasporedLigaSedmoVrijeme.text.toString()
+        val noviRasporedLigaSedmiDatum = binding.noviRasporedLigaSedmiDatum.text.toString()
+        val noviRasporedLigaSedmiDomacin = binding.noviRasporedLigaSedmiDomacin.text.toString()
+        val noviRasporedLigaSedmiGost = binding.noviRasporedLigaSedmiGost.text.toString()
+        val noviRasporedLigaSedmoVrijeme = binding.noviRasporedLigaSedmoVrijeme.text.toString()
 
-            //Create object
-            val tablicaRaspored = TablicaRaspored(0,noviRasporedLigaBrojKola,
-                noviRasporedLigaPrviDatum,noviRasporedLigaPrviDomacin,noviRasporedLigaPrviGost,noviRasporedLigaPrvoVrijeme,
-                noviRasporedLigaDrugiDatum,noviRasporedLigaDrugiDomacin,noviRasporedLigaDrugiGost,noviRasporedLigaDrugoVrijeme,
-                noviRasporedLigaTreciDatum,noviRasporedLigaTreciDomacin,noviRasporedLigaTreciGost,noviRasporedLigaTreceVrijeme,
-                noviRasporedLigaCetvrtiDatum,noviRasporedLigaCetvrtiDomacin,noviRasporedLigaCetvrtiGost,noviRasporedLigaCetvrtoVrijeme,
-                noviRasporedLigaPetiDatum,noviRasporedLigaPetiDomacin,noviRasporedLigaPetiGost,noviRasporedLigaPetoVrijeme,
-                noviRasporedLigaSestiDatum,noviRasporedLigaSestiDomacin,noviRasporedLigaSestiGost,noviRasporedLigaSestoVrijeme,
-                noviRasporedLigaSedmiDatum,noviRasporedLigaSedmiDomacin,noviRasporedLigaSedmiGost,noviRasporedLigaSedmoVrijeme)
+        //Create object
+        val tablicaRaspored = TablicaRaspored(
+            0,
+            noviRasporedLigaBrojKola,
+            noviRasporedLigaPrviDatum,
+            noviRasporedLigaPrviDomacin,
+            noviRasporedLigaPrviGost,
+            noviRasporedLigaPrvoVrijeme,
+            noviRasporedLigaDrugiDatum,
+            noviRasporedLigaDrugiDomacin,
+            noviRasporedLigaDrugiGost,
+            noviRasporedLigaDrugoVrijeme,
+            noviRasporedLigaTreciDatum,
+            noviRasporedLigaTreciDomacin,
+            noviRasporedLigaTreciGost,
+            noviRasporedLigaTreceVrijeme,
+            noviRasporedLigaCetvrtiDatum,
+            noviRasporedLigaCetvrtiDomacin,
+            noviRasporedLigaCetvrtiGost,
+            noviRasporedLigaCetvrtoVrijeme,
+            noviRasporedLigaPetiDatum,
+            noviRasporedLigaPetiDomacin,
+            noviRasporedLigaPetiGost,
+            noviRasporedLigaPetoVrijeme,
+            noviRasporedLigaSestiDatum,
+            noviRasporedLigaSestiDomacin,
+            noviRasporedLigaSestiGost,
+            noviRasporedLigaSestoVrijeme,
+            noviRasporedLigaSedmiDatum,
+            noviRasporedLigaSedmiDomacin,
+            noviRasporedLigaSedmiGost,
+            noviRasporedLigaSedmoVrijeme
+        )
 
-            mTablicaRasporedViewModel.addTablicaRaspored(tablicaRaspored)
-            Toast.makeText(requireContext(),"Successfully added", Toast.LENGTH_LONG).show()
+        mTablicaRasporedViewModel.addTablicaRaspored(tablicaRaspored)
+        Toast.makeText(requireContext(), "Successfully added", Toast.LENGTH_LONG).show()
 
     }
 }
