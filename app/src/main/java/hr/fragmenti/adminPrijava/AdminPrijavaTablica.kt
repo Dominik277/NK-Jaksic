d3bcc005f8d79663fd8ba487866df1101b7bd2dc
Dominik277 <dominikdoric277@gmail.com>
2021-04-13 17:11:46 +0200

    Dodana logika za admina u jos dva fragmenta.

--- app/src/main/java/hr/fragmenti/adminPrijava/AdminPrijavaTablica.kt
+++ app/src/main/java/hr/fragmenti/adminPrijava/AdminPrijavaTablica.kt
@@ -2,7 +2,9 @@ package hr.fragmenti.adminPrijava
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import hr.dominik.nkjaki.R
 import hr.dominik.nkjaki.databinding.AdminPrijavaTablicaBinding
@@ -16,7 +18,23 @@ class AdminPrijavaTablica: Fragment(R.layout.admin_prijava_tablica) {
         super.onViewCreated(view, savedInstanceState)
         binding = AdminPrijavaTablicaBinding.bind(view)
 
-        binding.gumbPrijavaAdminPrijaviSe.setOnClickListener {  }
-    }
+        binding.gumbPrijavaAdminPrijaviSe.setOnClickListener {
+            val korisnickoIme = binding.etPrijavaAdminKorisnickoIme.text.toString()
+            val lozinka = binding.etPrijavaAdminLozinka.text.toString()
+            val token = binding.etPrijavaAdminToken.text.toString()
 
+            if(korisnickoIme == "Dominik" && token == "token" && lozinka == "lozinka"){
+                val action = AdminPrijavaTablicaDirections.actionAdminPrijavaTablicaToUpdateTablicaTablicaFragment(args.currentTablicaArgs)
+                findNavController().navigate(action)
+            }else if (binding.etPrijavaAdminKorisnickoIme.text.toString().isEmpty()){
+                binding.etPrijavaAdminKorisnickoIme.error = "Ovo polje je obavezno!"
+            }else if (binding.etPrijavaAdminLozinka.text.toString().isEmpty()){
+                binding.etPrijavaAdminLozinka.error = "Ovo polje je obavezno!"
+            }else if(binding.etPrijavaAdminToken.text.toString().isEmpty()){
+                binding.etPrijavaAdminToken.error = "Ovo polje je obavezno!"
+            }else{
+                Toast.makeText(requireContext(),"Unjeli ste krive podatke!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
