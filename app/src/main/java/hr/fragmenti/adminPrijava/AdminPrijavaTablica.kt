3fc6d0e7e8cdc4d635b48012783ed1d389c26094
Dominik277 <dominikdoric277@gmail.com>
2021-04-13 16:58:30 +0200

    Dodana logika za admina u jos dva fragmenta.

--- app/src/main/java/hr/fragmenti/adminPrijava/AdminPrijavaTablica.kt
+++ app/src/main/java/hr/fragmenti/adminPrijava/AdminPrijavaTablica.kt
@@ -1,10 +1,22 @@
 package hr.fragmenti.adminPrijava
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import hr.dominik.nkjaki.R
+import hr.dominik.nkjaki.databinding.AdminPrijavaTablicaBinding
 
 class AdminPrijavaTablica: Fragment(R.layout.admin_prijava_tablica) {
 
+    private lateinit var binding: AdminPrijavaTablicaBinding
+    private val args by navArgs<AdminPrijavaTablicaArgs>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = AdminPrijavaTablicaBinding.bind(view)
+
+        binding.gumbPrijavaAdminPrijaviSe.setOnClickListener {  }
+    }
 
 }
