1193d9a615dbb282f0b3a8a89ecb8fa9f9c6ee86
Dominik277 <dominikdoric277@gmail.com>
2021-04-16 10:20:40 +0200

    Izbrisan ishod u potpunosti iz aplikacije.

--- app/src/main/java/hr/database/NKJaksicDatabase.kt
+++ app/src/main/java/hr/database/NKJaksicDatabase.kt
@@ -1,13 +1,9 @@
 package hr.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import hr.database.dao.*
 import hr.database.table.*
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
 
 @Database(
     entities = [Igraci::class,
@@ -29,30 +25,4 @@ abstract class NKJaksicDatabase : RoomDatabase() {
     abstract fun najboljiStrijelciDao(): NajboljiStrijelciDao
     abstract fun tablicaRasporedDao(): TablicaRasporedDao
     abstract fun tablicaRezultatiDao(): TablicaRezultatiDao
-
-    /*
-    companion object {
-        @Volatile
-        private var INSTANCE: NKJaksicDatabase? = null
-
-        @InternalCoroutinesApi
-        fun getDatabase(context: Context): NKJaksicDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    NKJaksicDatabase::class.java,
-                    "nk_jaksic_baza"
-                ).fallbackToDestructiveMigration()
-                    .allowMainThreadQueries()
-                    .build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
-     */
 }
