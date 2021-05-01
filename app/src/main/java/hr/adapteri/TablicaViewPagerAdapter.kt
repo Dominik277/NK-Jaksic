ff18937b86d17de9a0b60cbd51d889b657fc8bc6
Dominik277 <dominikdoric277@gmail.com>
2021-05-01 13:38:32 +0200

    Dodan viewpager i tablayout u fragment_tablica.xml i dodan TablicaViewPagerAdapter.kt.

---
+++ app/src/main/java/hr/adapteri/TablicaViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package hr.adapteri
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class TablicaViewPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+
+
+}
