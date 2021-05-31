7a5b636d06efe0be20cd03c28316b1c40b8219d9
Dominik277 <dominikdoric277@gmail.com>
2021-05-31 19:31:30 +0200

    Implementiran SplashScreen do kraja.

--- app/src/main/java/hr/SplashActivity.kt
+++ app/src/main/java/hr/SplashActivity.kt
@@ -1,7 +1,15 @@
 package hr
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import hr.dominik.nkjaki.R
 import hr.dominik.nkjaki.databinding.ActivitySplashBinding
 
@@ -14,5 +22,34 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.anim_splash)
+        binding.splashScreenTextView.animation = splashAnimation
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                    finish()
+                },1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+
     }
 }
