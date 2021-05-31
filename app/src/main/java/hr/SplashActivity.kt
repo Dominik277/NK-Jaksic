eed9a1a853746de15acdcf20d20187ca538e6464
Dominik277 <dominikdoric277@gmail.com>
2021-05-31 19:24:27 +0200

    Kreiran anim folder te dodan binding.

--- app/src/main/java/hr/SplashActivity.kt
+++ app/src/main/java/hr/SplashActivity.kt
@@ -3,10 +3,16 @@ package hr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hr.dominik.nkjaki.R
+import hr.dominik.nkjaki.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
